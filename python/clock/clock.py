017000194094f73110af99e39c4f7e8aa4827946
Ollie Ford <me@ojford.com>
2016-07-24 00:46:19 +0100

    implements some Python exercises to try exercism.io
    http://exercism.io/OJFord

---
+++ python/clock/clock.py
@@ -0,0 +1,25 @@
+class Clock:
+
+    def __init__(self, hour: int, minute: int):
+        self._hour = hour
+        self._minute = minute
+        self._normalise()
+
+    def __eq__(self, other) -> bool:
+        return (self._hour == other._hour) and (self._minute == other._minute)
+
+    def __ne__(self, other) -> bool:
+        return not self == other
+
+    def __str__(self) -> str:
+        return '{:02d}:{:02d}'.format(self._hour, self._minute)
+
+    def _normalise(self):
+        self._hour += self._minute // 60
+        self._hour %= 24
+        self._minute %= 60
+
+    def add(self, minutes: int):
+        self._minute += minutes
+        self._normalise()
+        return self
---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,7 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name: str='') -> str:
+    return 'Hello, {}!'.format(name or 'World')
